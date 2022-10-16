b0cd5aec6a2f8daf45137c4d56cc1cad27e78ee2
Replit user <>
2022-10-16 22:04:50 +0000

    new try

--- heap.c
+++ heap.c
@@ -32,8 +32,13 @@ void* heap_top(Heap* pq)
 
 
 
-void heap_push(Heap* pq, void* data, int priority){
+void heap_push(Heap* pq, void* data, int priority)
+{
+  heapElem elemento;
+  elemento->data = data;
+  elemento->priority = priority;
 
+  if()
 }
 
 

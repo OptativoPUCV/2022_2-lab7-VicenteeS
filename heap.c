5e4c879aa110b7be557cd4a3c96b632913a0ebd4
Replit user <>
2022-10-17 00:45:25 +0000

    new try

--- heap.c
+++ heap.c
@@ -42,7 +42,7 @@ void heap_push(Heap* pq, void* data, int priority)
   if(pq->size == pq->capac)
   {
     pq->capac = (pq->capac*2) + 1;
-    pq->heapArray = (heapArray) realloc (pq->heapArray, pq->capac); 
+    pq->heapArray = (heapElem *) realloc (pq->heapArray, pq->capac); 
   }
   pq->heapArray[pq->size] = elemento;
   

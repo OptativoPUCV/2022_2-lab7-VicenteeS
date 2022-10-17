b53d805447cda55129846f96250caf89a1af9792
Replit user <>
2022-10-17 02:22:53 +0000

    new try

--- heap.c
+++ heap.c
@@ -70,7 +70,7 @@ void heap_pop(Heap* pq)
   pq->heapArray[0] = pq->heapArray[pq->size];
 
   
-  while((actual * 2 <= pq->size) && ((pq->heapArray[actual].priority < pq->heapArray[(actual*2) +1].priority) || (pq->heapArray[actual].priority < pq->heapArray[(actual*2)+2].priority )))
+  while(((actual * 2)+2 <= pq->size) && ((pq->heapArray[actual].priority < pq->heapArray[(actual*2) +1].priority) || (pq->heapArray[actual].priority < pq->heapArray[(actual*2) +2].priority )))
   {
     if(pq->heapArray[(actual*2) +1].priority > pq->heapArray[(actual*2) +2].priority)
     {
